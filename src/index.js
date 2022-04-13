57f15cacaac2fe1c72e4a57bb1adc9061bd2a010
“Francis <notchera@gmail.comssh-keygen -t rsa -C notchera@gmail.com>
2022-04-13 20:03:19 +0100

    set up basic server

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ config();
 
 import server from "./server.js";
 
-const PORT = process.env.PORT || 5000;
-
-server.get("/", (req, res) => res.send("API up and running!"));
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.get("/", (req, res) => res.send("API up and running!"));
+
 io.on("connection", (socket) => {
   console.log("a user connected");
 });
+
+export default server;
