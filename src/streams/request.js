cf8346a32002795db8d0a48965883da7009750d7
“Francis <notchera@gmail.comssh-keygen -t rsa -C notchera@gmail.com>
2022-04-13 23:12:19 +0100

    set up write and read functionality across stream times for communication between controller and server

--- src/streams/request.js
+++ src/streams/request.js
@@ -8,8 +8,26 @@ export default class Request extends Writable {
     this._socket.emit("request", id, inbound);
   }
 
+  /* 
+  For processing multiple chunks at once.From the Node docs:
+  If a stream implementation is capable of processing 
+  multiple chunks of data at once, 
+  the writable._writev() method should be implemented.
+  */
+  _writev(data, next) {
+    this._socket.emit("inbound-pipes", this.id, data);
+    this._socket.conn.once("drain", () => {
+      next();
+    });
+  }
+
+  /*
+  This is a method for writing streams as well but
+  it gets deprioritized - for _writev (see above) - when a stream contains
+  several chunks.
+  */
   _write(data, _, next) {
-    this._socket, this.emit("pipe", this._id, data);
+    this._socket, this.emit("inbound-pipe", this._id, data);
     next();
   }
 }
--- src/streams/response.js
+++ src/streams/response.js
@@ -5,5 +5,29 @@ export default class Response extends Readable {
     super();
     this._id = id;
     this._socket = socket;
+
+    /* 
+    The handlePipe function processes simple chunks of data being piped -
+    ideally this would be data streamed via _write
+   */
+    const handlePipe = (id, data) => {
+      if (this._id !== id) return;
+      this.push(data);
+    };
+    /* 
+    handlePipes processes multiple chunks of data being piped -
+    ideally this would be data streamed via _writev
+   */
+    const handlePipes = (id, data) => {
+      if (this._id !== id) return;
+      data.forEach((chunk) => this.push(chunk));
+    };
+    this._socket.on("response", (id, data) => {
+      if (this._id !== id) return;
+      const { statusCode, statusMessage, headers } = data;
+      this.emit("response", statusCode, statusMessage, headers);
+    });
+    this._socket.on("outbound-pipe", handlePipe);
+    this._socket.on("outbound-pipes", handlePipes);
   }
 }
