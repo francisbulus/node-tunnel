ae74ccbc61dffa5cb34a337e69f7adc1fdf53a85
“Francis <notchera@gmail.comssh-keygen -t rsa -C notchera@gmail.com>
2022-04-28 19:14:52 +0100

    fresh build for compile

--- build/index.js
+++ build/index.js
@@ -1,10 +1,5 @@
 import { config } from "dotenv";
-
 config();
-
 import server from "./server.js";
-
-const PORT = process.env.PORT || 1337;
-//"0.0.0.0"
-server.listen(PORT);
+server.listen(process.env.PORT);
 //# sourceMappingURL=index.js.map
