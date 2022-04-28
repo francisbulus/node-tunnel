da14c0f5f7bf93f05bdf8e7690a0a54b16800a8c
“Francis <notchera@gmail.comssh-keygen -t rsa -C notchera@gmail.com>
2022-04-28 19:11:38 +0100

    _

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import { config } from "dotenv";
-
 config();
-
 import server from "./server.js";
-
-// const PORT = process.env.PORT || 1337;
-//"0.0.0.0"
-server.listen();
+server.listen(process.env.PORT);
