845aa90843bf4eceb5a692c0fec6fa64235e236a
“Francis <notchera@gmail.com>
2022-04-29 14:18:08 +0100

    new fix

--- src/utils/general-helpers/sockets.js
+++ src/utils/general-helpers/sockets.js
@@ -21,28 +21,32 @@ export const checkConnection = async (req, res, next, store, adapter) => {
   let socket;
   const access = roomAccessFromSession || roomAccessFromInput;
 
-  // experiment starts here
-  res.locals.socket = adapter;
-  next();
-  // experiment ends here
+  // // experiment starts here
+  // res.locals.socket = adapter;
+  // // res.locals.room = access;
+  // next();
 
-  // if (!access) {
-  //   res.sendFile("index.html", { root: "src/" + "public" });
-  //   return;
-  // } else {
-  //   const socketId = await store.get(access);
-  //   socket = io.sockets.sockets.get(socketId);
-  //   if (!roomAccessFromSession && socket) store.set(clientIp, access);
-  //   if (!socket) {
-  //     await store.del(access);
-  //     await store.del(clientIp);
-  //     res
-  //       .status(404)
-  //       .send("No socket connection found for the given room access key");
-  //     return;
-  //   }
-  //   res.locals.socket = socket;
-  //   // res.locals.room = access;
-  //   next();
-  // }
+  // experiment ends here
+  if (!access) {
+    res.sendFile("index.html", { root: "src/" + "public" }, (err) => {
+      if (err) {
+        res.end(500);
+      } else res.status(200);
+    });
+    return;
+  } else {
+    const socketId = await store.get(access);
+    socket = io.sockets.sockets.get(socketId);
+    if (!roomAccessFromSession && socket) store.set(clientIp, access);
+    if (!socket) {
+      await store.del(access);
+      await store.del(clientIp);
+      res
+        .status(404)
+        .send("No socket connection found for the given room access key");
+      return;
+    }
+    res.locals.socket = socket;
+    next();
+  }
 };
