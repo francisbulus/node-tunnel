d00a2cdb057b22214f4bd2acb4a4c8a57ca5058d
“Francis <notchera@gmail.com>
2022-05-06 16:26:00 +0100

    fix build issue resulting from package being read as a module

--- proxy/build/index.js
+++ proxy/build/index.js
@@ -1,6 +1,10 @@
-import { config } from "dotenv";
-config();
-import server from "./server.js";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const server_1 = __importDefault(require("./server"));
 const PORT = process.env.PORT || 1337;
-server.listen(PORT);
-//# sourceMappingURL=index.js.map
+server_1.default.listen(PORT);
